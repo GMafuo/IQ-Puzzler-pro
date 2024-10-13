1e4f306b50a1dc9c96a6531ff19f4febf7270cc9
Gmafuo <matheogirard@hotmail.com>
2024-10-13 21:44:00 +0200

    feat: ajout de nouveaux plateaux de jeu pour différents niveaux

---
+++ iq_solver_2d.py
@@ -0,0 +1,99 @@
+from typing import Tuple
+from base import Plateau, Direction, IqSolverBase, DirectionOrientation, DirectionRotation
+
+from pieces import EMPTY, StoneColor
+import time
+    
+
+BOARD_WIDTH = 11
+BOARD_HEIGHT = 5
+
+
+class IqSolver2d(IqSolverBase):
+    def _init_board(self) -> Plateau:
+        return [
+            [
+                [EMPTY for _ in range(BOARD_WIDTH)]
+                for _ in range(BOARD_HEIGHT)
+            ]
+        ]
+
+    def _initialiser_directions(self) -> Tuple[Direction, ...]:
+        return tuple(
+            (0, orientation, rotation) 
+            for orientation in (0, 1, 2, 3)
+            for rotation in (1, -1)
+        )
+
+
+GAME: int = "Expert1"
+TEST = False
+
+solver = IqSolver2d(file_name_color=f"{__file__}.{GAME}.solution-color.txt",
+                    file_name_text=f"{__file__}.{GAME}.solution.txt")
+
+
+def placer_pierre(color: StoneColor, direction: tuple[DirectionOrientation, DirectionRotation], depart: tuple[int, int]):
+    solver.placer_pierre(color, (0, *direction), depart=(*depart, 0))
+
+
+if GAME == "Starter1":  
+    placer_pierre("green", (0, 1), (1, 0))
+    placer_pierre("red", (2, -1), (4, 0))
+    placer_pierre("lime", (3, 1), (9, 1))
+    placer_pierre("lightblue", (2, 1), (2, 4))
+    placer_pierre("violet", (1, 1), (0, 0))
+    placer_pierre("lightgreen", (0, -1), (5, 1))
+    placer_pierre("pink", (2, 1), (6, 3))
+    placer_pierre("cyan", (0, 1), (5, 2))
+    placer_pierre("lightred", (3, 1), (1,3))
+elif GAME == "Starter2":  
+    placer_pierre("lightblue", (3, 1), (0, 2))
+    placer_pierre("red", (2, 1), (4, 2))
+    placer_pierre("violet", (3, 1), (6, 2))
+    placer_pierre("yellow", (2, -1), (9, 0))
+    placer_pierre("blue", (2, -1), (10, 0))
+    placer_pierre("lightgreen", (1, -1), (9, 2))
+    placer_pierre("cyan", (2, 1), (8, 2))
+    placer_pierre("pink", (2, 1), (9, 3))
+    placer_pierre("green", (3, 1), (5, 4))
+elif GAME == "Junior1":
+    placer_pierre("yellow", (2, -1), (3, 0))
+    placer_pierre("pink", (2, 1), (5, 0))
+    placer_pierre("lime", (1, -1), (6, 0))
+    placer_pierre("violet", (2, -1), (2, 3))
+    placer_pierre("cyan", (1, 1), (0, 3))
+    placer_pierre("lightred", (3, 1), (2,4))
+    placer_pierre("red", (1, -1), (2, 2))
+    placer_pierre("lightblue", (0, 1), (4, 2))
+elif GAME == "Junior2":
+    placer_pierre("yellow", (0, 1), (0, 0))
+    placer_pierre("violet", (3, -1), (3, 2))
+    placer_pierre("lightblue", (2, 1), (2, 4))
+    placer_pierre("blue", (1, -1), (0, 1))
+    placer_pierre("red", (2, 1), (3, 4))
+    placer_pierre("lightgreen", (3, -1), (4, 4))
+    placer_pierre("cyan", (0, 1), (5, 1))
+    placer_pierre("orange", (2, 1), (7, 4))
+elif GAME == "Expert1" :
+    placer_pierre("yellow", (0, 1), (0, 0))
+    placer_pierre("cyan", (2, 1), (1, 1))
+    placer_pierre("lightgreen", (2, -1), (2, 3))
+    placer_pierre("blue", (0, -1), (1, 3))
+    placer_pierre("green", (1, 1), (4, 0))
+    placer_pierre("lime", (1, 1), (4, 3))
+
+
+
+
+
+
+
+if TEST:
+    solver.print_board(no_dups=False)
+else:
+    solver.charger_plateau()
+    solver.resoudre()
+    solver.print_solutions()
+    solver.afficher_plateau()
+   
